8217a2c010eb277791d78362e268ba468640fc7b
Joe C <leclpp@hotmail.com>
2020-08-13 22:56:27 -0700

    This represents Part 7 of the tutorial - prepping for production. I made a vanilla backup of the project in dev/projects/mz1 Today, I added compression and helmet to app.js ...

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression = require('compression');
+var helmet = require('helmet');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +23,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(compression()); //use compression on all routes
+
+app.use(helmet()); //set appropriate HTTP headers to protect against web vulnerabilities
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
