9ab9848c2573d3653e1b2c848ab47d5a27bc5a65
Joe C <leclpp@hotmail.com>
2020-08-17 01:19:41 -0700

    Added changes for deployment to Heroku

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 
 //set up mongooose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://atlasAdmin:5EKSiqsJ6OOlkRSX@cluster0-1x7ph.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://atlasAdmin:5EKSiqsJ6OOlkRSX@cluster0-1x7ph.azure.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
